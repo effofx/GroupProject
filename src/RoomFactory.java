7f9a3d793e43b190b5a63c3eed0108cad90651e6
Sam Alhaqab <azenithsam@gmail.com>
2018-05-07 13:41:21 -0700

    - added static variables for the different room types - changed the parameter in createRoom() from ArrayList<String> to ArrayList<Amenities>. This simplified the logic to add amenities (got rid of the if-else logic) but added complexity to the child classes of Amenities in the from of a psuedo constructor

--- src/RoomFactory.java
+++ src/RoomFactory.java
@@ -1,42 +1,55 @@
+/**
+ May 8, 2018
+ Purpose: centralization for the creation different types of Room objects
+ Input: none
+ Output: various types of Room objects
+ **/
+
 import java.util.ArrayList;
 
 public class RoomFactory {
+    public static final String REGULAR = "regular";
+    public static final String DELUXE = "deluxe";
+    public static final String SUITE = "suite";
 
     public RoomFactory(){}
 
-    public Room createRoom(String roomType, ArrayList<String> amenities){
+    /**
+     * createRoom() first creates a room based on roomType
+     * then decorates the room with the various decorations
+     *
+     * @param roomType is the type of room to be instantiated
+     * @param amenities is the list of amenities the guest has requested
+     * @return
+     */
+    public Room createRoom(String roomType, ArrayList<Amenities> amenities){
         roomType = roomType.toLowerCase();
 
         Room room = null;
-        if(roomType.equals("regular")){
+        if(roomType.equals(REGULAR)){
             room = new RegularRoom();
         }
-        else if(roomType.equals("deluxe")){
+        else if(roomType.equals(DELUXE)){
             room = new DeluxeRoom();
         }
-        else if(roomType.equals("suite")){
+        else if(roomType.equals(SUITE)){
             room = new SuiteRoom();
         }
 
-        // adding amenities to the room
-        for (int i = 0; i < amenities.size(); i++) {
-            if(amenities.get(i).equals("foodbar")){
-                room = new FoodBar(room);
-            }
-            else if(amenities.get(i).equals("spa")){
-                room = new Spa(room);
-            }
-            else if(amenities.get(i).equals("autorefill")){
-                room = new AutoRefill(room);
-            }
-            else if(amenities.get(i).equals("shower")){
-                room = new AntiSprayShower(room);
-            }
-            else{
-                System.out.println("The amenity " + amenities + " does not exist");
-            }
+        // adds the amenities
+        for (Amenities amenity: amenities) {
+            room = amenity.setUp(room);
         }
         return room;
+    }
+
+    /**
+     * Overrided as per instructions, nothing really useful.
+     * @return
+     */
+    @Override
+    public String toString() {
+        return "This is the Room Factory";
     }
 }
 
